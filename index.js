1107c26707d1b3a6ff1e8563546be6ee576ea2fe
Mofizul Islam Shuvo <mofizul.shuvoislam@gmail.com>
2025-11-13 19:43:09 +0600

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(
 );
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   "mongodb+srv://Mofizul-The-Book-Heaven:9CJYg47RKm9SIRxT@cluster0.h2qhrdv.mongodb.net/?appName=Cluster0";
 
@@ -45,6 +45,38 @@ async function run() {
         result,
       });
     });
+
+    app.get("/my-books/:email", async (req, res) => {
+      const email = req.params.email; // get email from URL
+      try {
+        const books = await BookCollection.find({ userEmail: email }).toArray();
+        res.send(books); // send filtered books
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({
+          success: false,
+          message: "Failed to fetch books for this user",
+        });
+      }
+    });
+
+
+
+
+  
+    app.put("/Book-data/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body; 
+      const result = await BookCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      );
+      res.send({ success: result.modifiedCount > 0 });
+    });
+
+
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
